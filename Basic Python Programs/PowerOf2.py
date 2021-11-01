7f38e1f166a8df133b9b67bbdb17350caada2ff6
Pavan699 <pavannakate03@gmail.com>
2021-11-01 14:17:58 +0530

    [Pavan] Add : print power of 2 upto the Entered number

---
+++ Basic Python Programs/PowerOf2.py
@@ -0,0 +1,24 @@
+'''
+@Author: Pavan Nakate
+@Date: 2021-11-01 10:36
+@Last Modified by: Pavan Nakate
+@Last Modified time: None
+@Title : Power of 2 : Print the Power of 2 upto the entered number 
+'''
+#importing math library to use pow
+import math
+Number = int(input("Enter a range to get the power of two: "))
+
+# Checking if entered range is within the limit.
+if(0<Number):
+    for i in range (1,Number+1):      # Iteration from 1 to Number
+        print("2 ^ ",i," = ",pow(2,i)) 
+else:
+    print("Enter proper range")
+
+# Without using Pow i.e. using '**'
+if(0<Number):
+    for j in range (1,Number+1):
+        print("2 ^ ",j," = ",2**j)
+else:
+    print("Enter proper range")
