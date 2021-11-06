68ae24265b7ca6aedacc2f4685e4f623ccef553b
Pavan699 <pavannakate03@gmail.com>
2021-11-06 09:11:52 +0530

    [Pavan] Refactor: updating json file in inventary managment

--- Object Oriented Programs/InventoryDataManagement.py
+++ Object Oriented Programs/InventoryDataManagement.py
@@ -15,10 +15,11 @@ myjson_file = open('D:\Python\CFP-Class\Object Oriented Programs\Data.json','r')
 json_data = myjson_file.read()
 
 #Loading the data by using json.loads method
-json_obj = json.loads(json_data) #Data is in Object format
+json_obj = json.loads(json_data) 
 
 list = json_obj # there are 3 data's present so storing in the list format
 print(list)
+list_cal = [] # list to add the new object with bill
 
 for i in range(len(list)): #for loop for accessing the objects in list
     print("Details of the item => ")
@@ -27,27 +28,14 @@ for i in range(len(list)): #for loop for accessing the objects in list
     wg = float(list[i].get('weigth')) #conversion of string to float 
     print("Price : ",list[i].get('price'))
     pr = float(list[i].get('price'))  #conversion of string to float
-    print("Bill is : ",wg*pr) # calculating bill by using converted values 
+    bill = wg * pr
+    print("Bill is : ",bill) # calculating bill by using converted values 
+    #creating a dictonary for creating new object with the bill
+    dict = {"name" : list[i].get('name'),"weigth":list[i].get('weigth'),"price":list[i].get('price'),"bill":bill}
+    list_cal.append(dict)#Adding it into the new list
 
-#String Format by using dumps
-data_str = json.dumps(list)
-print(data_str)
-
-
-
-## Single Json Object
-
-#open and read the json file 
-my_details_file = open('D:\Python\CFP-Class\Object Oriented Programs\Details.json','r')
-person_data = my_details_file.read()
-
-person_obj = json.loads(person_data) # Load-String
-print("Person Details :")
-print("Name :",person_obj['name'])
-print("Address :",person_obj['address'])
-print("City :",person_obj['city'])
-print("Phone No. :",person_obj['phone'])
-
-#dumps => dump-String function
-person_str = json.dumps(person_obj)
-print(person_str)
+#Opening a new json file for printing new json object with its bill
+with open('Data_Cal.json','w') as f :
+    json.dump(list_cal,f,indent=2)
+#printing new dict-object
+print(list_cal)
